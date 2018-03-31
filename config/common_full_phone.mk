6c584a66beea0c27ab31cbe9bfb6c74a7d2f6db6
mydongistiny <jaysonedson@gmail.com>
2018-03-30 22:56:50 -0700

    benzo: Clean up art config and extra from common
    Change-Id: I846205e4a77346a8a54a777aab48e99a70d7eccb
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -3,8 +3,3 @@ $(call inherit-product, vendor/benzo/config/common.mk)
 
 $(call inherit-product, vendor/benzo/config/telephony.mk)
 
-# ART NoDebug
-export ART_BUILD_TARGET_NDEBUG := true
-export ART_BUILD_TARGET_DEBUG := false
-export ART_BUILD_HOST_NDEBUG := true
-export ART_BUILD_HOST_DEBUG := false
