8cbc5d7b77d51116daa1e780580eb00fa549e262
mydongistiny <jaysonedson@gmail.com>
2018-04-02 14:24:48 -0700

    products: Update marlin/sailfish build desc for OPM2.171019.029
    Change-Id: Ifbbb02810c3749af96e1024500b9cca179c9c37c
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- products/benzo_sailfish.mk
+++ products/benzo_sailfish.mk
@@ -21,8 +21,8 @@ PRODUCT_MANUFACTURER := Google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=sailfish \
-    PRIVATE_BUILD_DESC="sailfish-user 8.1.0 OPM1.171019.021 4565141 release-keys" \
-    BUILD_FINGERPRINT="google/sailfish/sailfish:8.1.0/OPM1.171019.021/4565141:user/release-keys"
+    PRIVATE_BUILD_DESC="sailfish-user 8.1.0 OPM2.171019.029 4657601 release-keys" \
+    BUILD_FINGERPRINT="google/sailfish/sailfish:8.1.0/OPM2.171019.029/4657601:user/release-keys"
 
 # Copy device specific prebuilt files.
 PRODUCT_COPY_FILES += \
