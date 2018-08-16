839fd4a8e7c0487d0fa5754d3db45b512f33dab7
mydongistiny <jaysonedson@gmail.com>
2018-08-16 14:47:36 -0700

    benzo: Add hiddenapi whitelist for Browser
    Since Browser is using platform apis it needs to be whitelisted for
    hiddenapi or else it will force close. Also add it to build.
    
    Change-Id: I8e637346a934c1453fb0149f64440cae986ec322
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -4,6 +4,14 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/benzo/overlay/common
 # Inherit art options
 include vendor/benzo/config/art.mk
 
+# Extra packages
+PRODUCT_PACKAGES += \
+    Browser
+
+# Copy hiddenapi whitelist
+PRODUCT_COPY_FILES += \
+    vendor/benzo/prebuilt/etc/sysconfig/benzo-hiddenapi-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/benzo-hiddenapi-whitelist.xml
+
 # Keyboard libs
 PRODUCT_COPY_FILES += \
     vendor/benzo/prebuilt/lib64/libjni_latinimegoogle.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libjni_latinimegoogle.so
