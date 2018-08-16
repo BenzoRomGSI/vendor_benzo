187cc46307fda06dcc7727294c1b2189ff0727ce
mydongistiny <jaysonedson@gmail.com>
2018-08-16 14:52:36 -0700

    benzo: Clean up and add init files and props
    Change-Id: I01adebcb750aec6e32b6ebef36bc6c2e60da595d
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -8,13 +8,33 @@ include vendor/benzo/config/art.mk
 PRODUCT_PACKAGES += \
     Browser
 
+# Props
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.selinux=1 \
+    ro.actionable_compatible_property.enabled=false \
+    ro.com.android.prov_mobiledata=false \
+    debug.performance.tuning=1 \
+    keyguard.no_require_sim=true
+
 # Copy hiddenapi whitelist
 PRODUCT_COPY_FILES += \
     vendor/benzo/prebuilt/etc/sysconfig/benzo-hiddenapi-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/benzo-hiddenapi-whitelist.xml
 
+# Copy all init rc files
+$(foreach f,$(wildcard vendor/benzo/prebuilt/etc/init/*.rc),\
+	$(eval PRODUCT_COPY_FILES += $(f):system/etc/init/$(notdir $f)))
+
 # Keyboard libs
 PRODUCT_COPY_FILES += \
     vendor/benzo/prebuilt/lib64/libjni_latinimegoogle.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libjni_latinimegoogle.so
+
+# Use ccache
+USE_CCACHE := true
+# ThinLTO cache
+USE_THINLTO_CACHE=true
+
+# SystemUITests
+EXCLUDE_SYSTEMUI_TESTS := true
 
 # Inherit common product build prop overrides
 -include vendor/benzo/config/versions.mk
